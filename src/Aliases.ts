7ea27a7dda05ad7909deaa3b08a09df7cfd696f8
Muhammad Rendra <mnrendra@users.noreply.github.com>
2024-07-03 02:44:48 +0800

    feat: api descriptions (#10)
    issues:
    * feat: api descriptions (#9)
    
    commits:
    * feat: api descriptions (62463c0)

--- src/Aliases.ts
+++ src/Aliases.ts
@@ -1,5 +1,11 @@
 import type { Alias } from './types'
 
+/**
+ * A TypeScript type to validate the input of `@mnrendra/alias-resolver` or the
+ * output of `@mnrendra/tsconfig-alias-parser`.
+ *
+ * @see https://www.npmjs.com/package/@mnrendra/types-aliases
+ */
 export type Aliases = Alias[]
 
 export default Aliases
